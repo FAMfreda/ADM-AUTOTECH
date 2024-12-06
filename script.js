4c347afefddadaeff4919d63ef6b6d083b3cb0a1
FAMfreda <mbordfreda@gmail.com>
2024-12-06 14:48:50 +0000

    nav bar

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("JavaScript is connected successfully!");
+
+
+
+
+
+
+
+const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
